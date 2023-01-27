7f46c5b071a33e4746da857d7205beae492a0161
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-01-27 12:37:59 +0600

    Add and remove product done

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,31 +1,35 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../features/cart/cartSlice";
+import { useLocation } from "react-router-dom";
+import { addToCart, removeFromCart } from "../features/cart/cartSlice";
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+  const {pathname} = useLocation();
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      key={product._id}
-    >
+      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'>
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
       </div>
       <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
+      <p className='text-center font-semibold mb-1'>Rating: {product.rating}</p>
+      {product.quantity && <p className='text-center font-semibold mb-3'>Quantity: {product.quantity}</p>}
       <div className=' flex-1'>
         <ul className='space-y-2'>
-          {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+          {product.keyFeature.map((feature, index) => {
+            return <li className='text-sm' key={index}>{feature}</li>;
           })}
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button onClick={() => dispatch(addToCart(product))} className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+        {!pathname.includes('cart') && <button onClick={() => dispatch(addToCart(product))} className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
-        </button>
+        </button>}
+        {pathname.includes('cart') && <button onClick={() => dispatch(removeFromCart(product))} className='bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+          Remove
+        </button>}
         <button
           title='Add to wishlist'
           className='bg-indigo-500  py-1 px-2 rounded-full'
